66825e4de5a4b95d1756db421a3a69f1bd00dd6a
Sara Bolduc <sara.s.bolduc@gmail.com>
2019-10-19 17:44:13 -0700

    adds comments to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,26 +41,36 @@ var players = [
 // FUNCTIONS
 //=========================
 function start(){
+    //iterates over each object in array players
    $.each(players, function(){
+       //creates a new dive for each objedt
         var newDiv = $("<div></div>").addClass("charDiv rounded");
+        
+        //iterate over each key and value in the given object
        $.each(this, function(name, value){
             if(name === "name"){
                 // console.log("name = "+ value);
+                //If key === name: create new p tag of character Name, add a class, & append to div
                 $("<p></p>").text(value).addClass("charName").appendTo(newDiv);
             };
 
             if(name === "img"){
                 // console.log("img src= "+ value);
+                //if key === img: create new img tag, assign src to value, add class, & append to div
                 $("<img>").attr("src", value).addClass("charImg").appendTo(newDiv)
             };
             
             if(name === "healthPoint"){
                 // console.log("hp = "+ value);
+                //if key === hp: create new p tag of hp value, add a class, & append to div
                 $("<p></p>").text(value).addClass("charHP").appendTo(newDiv);
             }
+            //add all character divs to DOM in #players div
             $("#players").append(newDiv);
        });
    });
+
+
 }
 
 
